50521b768cadced724d98549cf40b6a675ad6620
Andy <109441070+andy-gil@users.noreply.github.com>
2022-07-16 16:30:28 -0600

    Initial commit

---
+++ assign2/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace assign2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.resetButton = new System.Windows.Forms.Button();
+            this.rollButton = new System.Windows.Forms.Button();
+            this.lblGuessEnter = new System.Windows.Forms.Label();
+            this.lblPlayed = new System.Windows.Forms.Label();
+            this.lblWon = new System.Windows.Forms.Label();
+            this.lblLoss = new System.Windows.Forms.Label();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.ResultsOfDice = new System.Windows.Forms.RichTextBox();
+            this.numGuessed = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(248, 297);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(85, 33);
+            this.resetButton.TabIndex = 0;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // rollButton
+            // 
+            this.rollButton.Location = new System.Drawing.Point(248, 238);
+            this.rollButton.Name = "rollButton";
+            this.rollButton.Size = new System.Drawing.Size(85, 30);
+            this.rollButton.TabIndex = 1;
+            this.rollButton.Text = "Roll";
+            this.rollButton.UseVisualStyleBackColor = true;
+            this.rollButton.Click += new System.EventHandler(this.rollButton_Click);
+            // 
+            // lblGuessEnter
+            // 
+            this.lblGuessEnter.AutoSize = true;
+            this.lblGuessEnter.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGuessEnter.Location = new System.Drawing.Point(18, 166);
+            this.lblGuessEnter.Name = "lblGuessEnter";
+            this.lblGuessEnter.Size = new System.Drawing.Size(206, 24);
+            this.lblGuessEnter.TabIndex = 3;
+            this.lblGuessEnter.Text = "Enter your guess (1-6): ";
+            // 
+            // lblPlayed
+            // 
+            this.lblPlayed.AutoSize = true;
+            this.lblPlayed.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayed.Location = new System.Drawing.Point(18, 23);
+            this.lblPlayed.Name = "lblPlayed";
+            this.lblPlayed.Size = new System.Drawing.Size(228, 24);
+            this.lblPlayed.TabIndex = 5;
+            this.lblPlayed.Text = "Number of Times Played: ";
+            // 
+            // lblWon
+            // 
+            this.lblWon.AutoSize = true;
+            this.lblWon.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWon.Location = new System.Drawing.Point(18, 63);
+            this.lblWon.Name = "lblWon";
+            this.lblWon.Size = new System.Drawing.Size(211, 24);
+            this.lblWon.TabIndex = 6;
+            this.lblWon.Text = "Number of Times Won: ";
+            // 
+            // lblLoss
+            // 
+            this.lblLoss.AutoSize = true;
+            this.lblLoss.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoss.Location = new System.Drawing.Point(18, 108);
+            this.lblLoss.Name = "lblLoss";
+            this.lblLoss.Size = new System.Drawing.Size(205, 24);
+            this.lblLoss.TabIndex = 7;
+            this.lblLoss.Text = "Number of Times Lost: ";
+            // 
+            // pbImage
+            // 
+            this.pbImage.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbImage.BackgroundImage")));
+            this.pbImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbImage.Location = new System.Drawing.Point(38, 213);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(154, 135);
+            this.pbImage.TabIndex = 8;
+            this.pbImage.TabStop = false;
+            // 
+            // ResultsOfDice
+            // 
+            this.ResultsOfDice.Location = new System.Drawing.Point(12, 383);
+            this.ResultsOfDice.Name = "ResultsOfDice";
+            this.ResultsOfDice.Size = new System.Drawing.Size(617, 167);
+            this.ResultsOfDice.TabIndex = 9;
+            this.ResultsOfDice.Text = "";
+            // 
+            // numGuessed
+            // 
+            this.numGuessed.Location = new System.Drawing.Point(248, 167);
+            this.numGuessed.Name = "numGuessed";
+            this.numGuessed.Size = new System.Drawing.Size(100, 22);
+            this.numGuessed.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 562);
+            this.Controls.Add(this.numGuessed);
+            this.Controls.Add(this.ResultsOfDice);
+            this.Controls.Add(this.pbImage);
+            this.Controls.Add(this.lblLoss);
+            this.Controls.Add(this.lblWon);
+            this.Controls.Add(this.lblPlayed);
+            this.Controls.Add(this.lblGuessEnter);
+            this.Controls.Add(this.rollButton);
+            this.Controls.Add(this.resetButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Button rollButton;
+        private System.Windows.Forms.Label lblGuessEnter;
+        private System.Windows.Forms.Label lblPlayed;
+        private System.Windows.Forms.Label lblWon;
+        private System.Windows.Forms.Label lblLoss;
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.RichTextBox ResultsOfDice;
+        private System.Windows.Forms.TextBox numGuessed;
+    }
+}
+
---
+++ assign2/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Need to include to use threading
+using System.Threading;
+
+namespace assign2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Attributes
+
+        /// <summary>
+        /// Needed for games, wins, and losses
+        /// </summary>
+        public int iNumOfGames = 0;
+        public int iNumOfWins = 0;
+        public int iNumOfLoss = 0;
+
+        /// <summary>
+        /// the amount of frequency each number shows up
+        /// </summary>
+        private int iFrequency1 = 0;
+        private int iFrequency2 = 0;
+        private int iFrequency3 = 0;
+        private int iFrequency4 = 0;
+        private int iFrequency5 = 0;
+        private int iFrequency6 = 0;
+
+        /// <summary>
+        /// the percentage for each number
+        /// </summary>
+        private double dPercentage1 = 0; 
+        private double dPercentage2 = 0; 
+        private double dPercentage3 = 0; 
+        private double dPercentage4 = 0; 
+        private double dPercentage5 = 0; 
+        private double dPercentage6 = 0;
+
+        /// <summary>
+        /// the total amount of guesses for each number
+        /// </summary>
+        private int iTotalGuess1 = 0;
+        private int iTotalGuess2 = 0;
+        private int iTotalGuess3 = 0;
+        private int iTotalGuess4 = 0;
+        private int iTotalGuess5 = 0;
+        private int iTotalGuess6 = 0;
+
+        private int iRandomNum;
+
+        #endregion
+
+
+        /// <summary>
+        /// the guess the user input
+        /// </summary>
+        private int iUserNumGuess;
+
+        ///
+        ///Create a random number object
+        ///
+        Random rndNumber = new Random();
+        
+
+        /// <summary>
+        /// This method resets all the values back to 0
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            iNumOfGames = 0;
+            iNumOfWins = 0;
+            iNumOfLoss = 0;
+
+            iFrequency1 = 0;
+            iFrequency2 = 0;
+            iFrequency3 = 0;
+            iFrequency4 = 0;
+            iFrequency5 = 0;
+            iFrequency6 = 0;
+
+            iTotalGuess1 = 0;
+            iTotalGuess2 = 0;
+            iTotalGuess3 = 0;
+            iTotalGuess4 = 0;
+            iTotalGuess5 = 0;
+            iTotalGuess6 = 0;
+
+            dPercentage1 = 0;
+            dPercentage2 = 0;
+            dPercentage3 = 0;
+            dPercentage4 = 0;
+            dPercentage5 = 0;
+            dPercentage6 = 0;
+
+            lblWon.Text = "Number of Times Won: " + iNumOfWins.ToString();
+            lblLoss.Text = "Number of Times Lost: " + iNumOfLoss.ToString();
+            lblPlayed.Text = "Number of Times Played: " + iNumOfGames.ToString();
+            ResultsOfDice.Text = "Face\t" + "Freq\t" + "Percemt\t" + "Number of Guesses\n"
+                                + "  1\t  " + iFrequency1 + "  \t  " + dPercentage1 + "%\t\t" + iTotalGuess1 + "\n"
+                                + "  2\t  " + iFrequency2 + "  \t  " + dPercentage2 + "%\t\t" + iTotalGuess2 + "\n"
+                                + "  3\t  " + iFrequency3 + "  \t  " + dPercentage3 + "%\t\t" + iTotalGuess3 + "\n"
+                                + "  4\t  " + iFrequency4 + "  \t  " + dPercentage4 + "%\t\t" + iTotalGuess4 + "\n"
+                                + "  5\t  " + iFrequency5 + "  \t  " + dPercentage5 + "%\t\t" + iTotalGuess5 + "\n"
+                                + "  6\t  " + iFrequency6 + "  \t  " + dPercentage6 + "%\t\t" + iTotalGuess6;
+        }
+
+        /// <summary>
+        /// This method creates a random number and checks if it matches the user guess
+        /// it checks to make sure the number inputted is 1-6
+        /// tallys the number of guess, frequency, and percentage
+        /// also display the picture of a dice roll
+        /// </summary>
+        private void rollButton_Click(object sender, EventArgs e)
+        {
+            //Using the random number object create two random numbers between 1 and 6
+            iRandomNum = rndNumber.Next(1, 7);
+
+            iUserNumGuess = Int32.Parse(numGuessed.Text);
+
+            /// <summary>
+            /// if the user guess anything other than 1-6 the game closes
+            /// </summary>
+            //if(iUserNumGuess > 6 || iUserNumGuess < 1)
+            //{
+            //    lblGuessEnter.Text = "Enter your guess (1-6): " + iNumOfGames.ToString() + "Please enter (1-6) only";
+            //    iRandomNum = rndNumber.Next(1, 7);
+
+            //    iUserNumGuess = Int32.Parse(numGuessed.Text);
+            //}
+
+
+            /// <summary>
+            /// Checks if the random number matches the user guess
+            /// if so the number of wins and number of games increments by 1
+            /// it also displays the number of wins and number of games in the text box
+            /// if the random number does not match the user guess
+            /// the number of losses and number of games increment by 1
+            /// then displays the the number of losses and games in the text box
+            /// </summary>
+            if (iRandomNum == iUserNumGuess)
+            {
+                ++iNumOfWins;
+                ++iNumOfGames;
+                lblPlayed.Text = "Number of Times Played: " + iNumOfGames.ToString();
+                lblWon.Text = "Number of Times Won: " + iNumOfWins.ToString();
+            }
+            else
+            {
+                ++iNumOfLoss;
+                ++iNumOfGames;
+                lblPlayed.Text = "Number of Times Played: " + iNumOfGames.ToString();
+                lblLoss.Text = "Number of Times Lost: " + iNumOfLoss.ToString();
+            }
+
+            /// <summary>
+            /// Using the user guess,
+            /// this tallys the amount of total guess for each number
+            /// e.g. user guesses 1, the total guess for totalguess1 goes up 1 
+            /// </summary>
+            switch (iUserNumGuess)
+            {
+                case 1:
+                    ++iTotalGuess1;
+                    break;
+                case 2:
+                    ++iTotalGuess2;
+                    break;
+                case 3:
+                    ++iTotalGuess3;
+                    break;
+                case 4:
+                    ++iTotalGuess4;
+                    break;
+                case 5:
+                    ++iTotalGuess5;
+                    break;
+                case 6:
+                    ++iTotalGuess6;
+                    break;
+            }
+
+            /// <summary>
+            /// increasing the amount of the frequency when its the result of the random number
+            /// </summary>
+            switch (iRandomNum)
+            {
+                case 1:
+                    ++iFrequency1;
+                    break;
+                case 2:
+                    ++iFrequency2;
+                    break;
+                case 3:
+                    ++iFrequency3;
+                    break;
+                case 4:
+                    ++iFrequency4;
+                    break;
+                case 5:
+                    ++iFrequency5;
+                    break;
+                case 6:
+                    ++iFrequency6;
+                    break;
+            }
+
+            /// <summary>
+            /// Calculating the percentage of each frequency
+            /// </summary>
+            dPercentage1 = Math.Round((double) iFrequency1 / iNumOfGames * 100, 2);
+            dPercentage2 = Math.Round((double) iFrequency2 / iNumOfGames * 100, 2);
+            dPercentage3 = Math.Round((double) iFrequency3 / iNumOfGames * 100, 2);
+            dPercentage4 = Math.Round((double) iFrequency4 / iNumOfGames * 100, 2);
+            dPercentage5 = Math.Round((double) iFrequency5 / iNumOfGames * 100, 2);
+            dPercentage6 = Math.Round((double) iFrequency6 / iNumOfGames * 100, 2);
+
+            /// <summary>
+            /// The results of frequency, percentage, and total games shown
+            /// </summary>
+            ResultsOfDice.Text = "Face\t" + "Freq\t" + "Percemt\t" + "Number of Guesses\n"
+                                + "  1\t  " + iFrequency1 + "  \t  " + dPercentage1 + "%\t\t" + iTotalGuess1 + "\n"
+                                + "  2\t  " + iFrequency2 + "  \t  " + dPercentage2 + "%\t\t" + iTotalGuess2 + "\n"
+                                + "  3\t  " + iFrequency3 + "  \t  " + dPercentage3 + "%\t\t" + iTotalGuess3 + "\n"
+                                + "  4\t  " + iFrequency4 + "  \t  " + dPercentage4 + "%\t\t" + iTotalGuess4 + "\n"
+                                + "  5\t  " + iFrequency5 + "  \t  " + dPercentage5 + "%\t\t" + iTotalGuess5 + "\n"
+                                + "  6\t  " + iFrequency6 + "  \t  " + dPercentage6 + "%\t\t" + iTotalGuess6;
+
+            //Randomly change the dice picture
+            pbImage.SizeMode = PictureBoxSizeMode.StretchImage;
+            for (int i = 1; i < 7; i++)
+                {
+                    int j = rndNumber.Next(1,7);
+                    pbImage.Image = Image.FromFile("die" + j.ToString() + ".gif");
+                    pbImage.Refresh();
+                    Thread.Sleep(300);
+                }
+        }
+
+    }
+}
